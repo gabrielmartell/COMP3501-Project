144ca2750846a4487e233bf4f064d1e4f6c44d18
Uzonna Alexander <uzonna101@outlook.com>
2023-12-07 09:11:18 -0500

    Small Change

--- Project/path_config.h
+++ Project/path_config.h
@@ -1,1 +1,1 @@
-#define MATERIAL_DIRECTORY "C:\\Users\\itsmy\\Desktop\\3501\\COMP3501-Project-hungry-ai\\Project"
+#define MATERIAL_DIRECTORY "C:\\Users\\uzonn\\Downloads\\University\\2501\\COMP 2501\\Project\\COMP3501-Project\\Project"
